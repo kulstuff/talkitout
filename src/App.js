8c74fda9ee43b656d65bead0c4915485ae70033e
aadi <aadiyaara@gmail.com>
2019-04-10 10:35:51 +0530

    Structured

--- src/App.js
+++ src/App.js
@@ -1,19 +1,31 @@
-import React, { Component } from 'react';
-import { Route, BrowserRouter } from 'react-router-dom';
+// Basic React
+import React, { Component } from 'react'
+import { Route, BrowserRouter } from 'react-router-dom'
 
 // Styling
-import './styles/sass/main.scss';
+import './styles/sass/main.scss'
 
 // Components
-import Home from './Components/Home';
+import Home from './Components/Foundational/Home'
+import Call from './Components/Foundational/Call'
 
 class App extends Component {
+
+    state = {
+        username: null,
+        email: null,
+        uId: null,
+        status: null,
+        password: null,
+    }
+
     render() {
         return (
             <React.Fragment>
                 <BrowserRouter>
                     <React.Fragment>
                         <Route path='/' component={Home} />
+                        <Route path='/call' component={Call} />
                     </React.Fragment>
                 </BrowserRouter>
             </React.Fragment>
