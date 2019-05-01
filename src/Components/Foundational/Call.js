686fadbe0f4cb9f799ad876ab96439317739ec56
aadi <aadiyaara@gmail.com>
2019-05-01 10:38:33 +0530

    Opentok integration init

--- src/Components/Foundational/Call.js
+++ src/Components/Foundational/Call.js
@@ -29,12 +29,12 @@ class Call extends Component {
         projectId: "talkitout-3f3b7",
         storageBucket: "talkitout-3f3b7.appspot.com",
         messagingSenderId: "206647692136"
-    };
+    }
 
     yourId
     pc = {}
 
-    database;
+    database
 
     static contextType = AuthContext
 
@@ -42,7 +42,7 @@ class Call extends Component {
         
         if(true && this.props.secret) {
             console.log('Seeking with props: ', this.props)
-            var servers = {'iceServers': [{'urls': 'stun:stun.services.mozilla.com'}, {'urls': 'stun:stun.l.google.com:19302'}, {'urls': 'turn:numb.viagenie.ca','credential': this.props.secret,'username': 'talkitout.kulstuff@gmail.com'}]};
+            var servers = {'iceServers': [{'urls': 'stun:stun.services.mozilla.com'}, {'urls': 'stun:stun.l.google.com:19302'}]}
             
             this.pc = new RTCPeerConnection(servers)
             
@@ -55,49 +55,45 @@ class Call extends Component {
                 messagingSenderId: "206647692136"
             }
             
-            this.yourId = Math.floor(Math.random()*1000000000);
+            this.yourId = Math.floor(Math.random()*1000000000)
             var yourVideo = document.getElementById('yourVideo')
             var friendsVideo = document.getElementById('friendsVideo')
             
             navigator.mediaDevices.getUserMedia({audio:true, video:true})
                 .then(stream => yourVideo.srcObject = stream)
-                .then(stream => this.pc.addStream(stream));
-            console.log('myVideo Object: ', yourVideo);
-            Firebase.initializeApp(this.config);
+                .then(stream => this.pc.addStream(stream))
+            console.log('myVideo Object: ', yourVideo)
+            Firebase.initializeApp(this.config)
             
-            console.log('Component Did Mount PC: ', this.pc);
-            this.database = Firebase.database().ref();
+            console.log('Component Did Mount PC: ', this.pc)
+            this.database = Firebase.database().ref()
             //Create an account on Viagenie (http://numb.viagenie.ca/), and replace {'urls': 'turn:numb.viagenie.ca','credential': 'websitebeaver','username': 'websitebeaver@email.com'} with the information from your account
-            this.pc.onicecandidate = (event => event.candidate?this.sendMessage(this.yourId, JSON.stringify({'ice': event.candidate})):console.log("Sent All Ice") );
-            this.pc.onaddstream = (event => friendsVideo.srcObject = event.stream);
+            this.pc.onicecandidate = (event => event.candidate?this.sendMessage(this.yourId, JSON.stringify({'ice': event.candidate})):console.log("Sent All Ice") )
+            this.pc.onaddstream = (event => friendsVideo.srcObject = event.stream)
 
-            this.database.on('child_added', this.readMessage);
+            this.database.on('child_added', this.readMessage)
         }
     }
-
-    // componentDidMount = () => {
-    //     //Create an account on Firebase, and use the credentials they give you in place of the following 
-
     
     sendMessage = (senderId, data) => {
-        var msg = this.database.push({ sender: senderId, message: data });
-        msg.remove();
+        var msg = this.database.push({ sender: senderId, message: data })
+        msg.remove()
         console.log('Reaches Here')
     }
 
     readMessage = (data) => {
-        var msg = JSON.parse(data.val().message);
-        var sender = data.val().sender;
+        var msg = JSON.parse(data.val().message)
+        var sender = data.val().sender
         if (sender != this.yourId) {
             if (msg.ice != undefined)
-                this.pc.addIceCandidate(new RTCIceCandidate(msg.ice));
+                this.pc.addIceCandidate(new RTCIceCandidate(msg.ice))
             else if (msg.sdp.type == "offer")
                 this.pc.setRemoteDescription(new RTCSessionDescription(msg.sdp))
                 .then(() => this.pc.createAnswer())
                 .then(answer => this.pc.setLocalDescription(answer))
-                .then(() => this.sendMessage(this.yourId, JSON.stringify({'sdp': this.pc.localDescription})));
+                .then(() => this.sendMessage(this.yourId, JSON.stringify({'sdp': this.pc.localDescription})))
             else if (msg.sdp.type == "answer")
-                this.pc.setRemoteDescription(new RTCSessionDescription(msg.sdp));
+                this.pc.setRemoteDescription(new RTCSessionDescription(msg.sdp))
         }
         console.log('Reaches Here')
     }
@@ -107,7 +103,7 @@ class Call extends Component {
     showFriendsFace = () => {
         this.pc.createOffer()
             .then(offer => this.pc.setLocalDescription(offer) )
-            .then(() => this.sendMessage(this.yourId, JSON.stringify({'sdp': this.pc.localDescription})) );
+            .then(() => this.sendMessage(this.yourId, JSON.stringify({'sdp': this.pc.localDescription})) )
     }
 
     // startFlow = () => {
